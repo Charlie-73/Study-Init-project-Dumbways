5a1c708bbcc678ba7793081e346e2866eb74a92e
Charlie-73 <88079662+Charlie-73@users.noreply.github.com>
2021-10-05 09:11:50 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require("http")
+const express =  require("express")
+const path = require("path")
+
+const app = express()
+const hbs = require("hbs")
+
+
+//register partials
+hbs.registerPartials(path.join(__dirname, "views/partials"))
+
+app.use(express.static('express'))
+app.use("/static", express.static(path.join(__dirname, "public")))
+//app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+
+app.use(express.urlencoded({ extended: false }))
+
+//set views location to app
+app.set("views", path.join(__dirname, "views"))
+
+
+app.set("view engine", "hbs")
+
+//home render
+app.get("/", function(req, res) {
+    res.render("index", {title: "Music", isLogin: ""})
+})
+
+//render login
+app.get("/", function(req, res) {
+    res.render("login", {title: "login", isLogin: true })
+})
+const server = http.createServer(app)
+const port = 8080
+server.listen(port, () => {
+    console.log("server running on ", port)
+})
